192c693d98a3312b03405a7d3115b770f58843d6
Muhammad Yasir <145558820+realmyasir@users.noreply.github.com>
2023-11-28 11:04:52 +0500

    Yes
    Python Dictionary:
    
    Description: A dictionary in Python is an unordered collection of key-value pairs. It is defined using curly braces {}, with each key-value pair separated by a colon (:). Dictionaries are versatile data structures used to store and retrieve data quickly based on a unique key.
    Python Modules:
    
    Description: A module in Python is a file containing Python definitions and statements. It serves as a way to organize and reuse code. Modules can include variables, functions, and classes that can be imported into other Python scripts or modules. This promotes code modularity and reusability.

---
+++ Day 7 Dictionary and Modules.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 28 09:27:03 2023
+
+@author: Web 123
+"""
+
+# capital={'yasir':'bro','jam':'br',"aon":'brot','Ak':'Big',}
+
+# capital.get('aon')
+# capital['Ali']='small'
+
+# capital.items()
+# print(capital)
+
+# for country in capital:
+#     print(country)
+
+# for country,bro in capital.items():
+#     print(f'The Capital of {country}, is {bro} ')
+    
+# print(capital.keys())
+# print(capital.values()) 
+
+
+# if 'ak' in capital:
+#     print('it Contain Ak')
+
+
+# capital = {'yasir': 'bro', 'jam': 'br', 'aon': 'brot', 'Ak': 'Big'}
+
+# if 'Ak' in capital:
+#     print('It contains Ak')
+
+
+# l=[1,2,3,4,5,6,7,6,8,4,6]
+# print(6 in  l)
+ 
+# letter count in varibales
+
+# u='''
+# # "sdfhjfflksfjhjjkjkkj"
+# # "sdjjkkjxbnnmzxjh"
+# # "sfjhjhjhxcznbmnsfh"
+# # "sfdfjhjhsjkfnbxc"
+# # "njjhjhsfjhjksf"
+# # "jahhjsfajbhssbnsf"
+# # "sfajhhjhgghsfbnsf"
+# # "sfjjhsjjhsf"
+# # "shjhjjsfjsf"
+# werklesdkljdg
+# dgkjkjjker
+# enkkcfkmer
+# ermllkkljgf
+# erknljkjkle
+
+# '''
+# letter_count={}
+# for letter in u:
+#     letter_count[letter.lower()]=letter_count.get(letter,0)+1
+# # letter['m']=1    
+# print(letter_count)
+
+
+
+import matplotlib.pyplot as plt
+
+
+# # x,y = zip(*letter_count.items())
+# # plt.bar(x,y)
+# # plt.show()
+
+# # a,b = zip(*letter_count.items())
+
+# letter_count_clean={}
+# for k,v in letter_count.items():
+#     if k.isalpha():
+#         letter_count_clean[k]=v
+# print(letter_count_clean)
+
+# x,y = zip(*letter_count.items())
+# plt.bar(x,y)
+# plt.show()
+
+l1=[1,2,3,4,5]
+l2=['a','b','c','d']
+# joind=list(zip(l1,l2))
+print(joind)
+
+i,j=zip(*joind)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
